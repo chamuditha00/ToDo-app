6f3e3887dd36bf1bf6c055cd9b638114f300beba
chamuditha00 <indushangunawardene@gmail.com>
2024-06-05 15:02:20 +0530

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+**Flutter with Node backend** is a simple project to demonstrate how to use Flutter with Node backend.
+
+*it is simple ToDo app*
+
+**this is the architecture of the project:**
+
---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
